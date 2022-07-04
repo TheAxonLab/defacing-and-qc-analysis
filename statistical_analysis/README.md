c405507d405d4981848b6ae01401c5878351c10f
cprovins <celine.provins@unil.ch>
2022-07-04 16:08:32 +0200

    fix : isolate the simulate_data in a function so it can be called easily in all the notebooks

--- statistical_analysis/README.md
+++ statistical_analysis/README.md
@@ -1,7 +1,8 @@
 ## Statistical analysis associated to the defacing pre-registration.
 
 
-- `SimulateDefacedRatings.ipnyb` is a notebook to simulate raters manually assigning quality grades to subjects in original and defaced conditions. The simulated data have a positive bias towards defaced ratings.
+- `simulate_data.R` is a script to simulate raters manually assigning quality grades to subjects in original and defaced conditions. The simulated data have a positive bias towards defaced ratings.
+- `VisualizeRatings.ipnyb` is a notebook to visualize the distributions of ratings in the defaced vs original conditions. 
 - `OrderedLogisticRegression.ipynb` is a notebook to run ordered logistic regression. It is a simpler model than mixed effect regression because it doesn't model between-rater variability. I used it to gain some understanding about logistic regression and continuation ratio assumption.
 - `MixedEffectRegression.ipynb` is the notebook to run mixed effect regression. This is the final analysis on which inference about the existence of a bias linked to defacing will be based.
 
---
+++ statistical_analysis/simulate_data.R
@@ -0,0 +1,63 @@
+simulate_data <- function(n_rated, n_sub, n_rater, perc_biased, file=""){
+    #' Simulate human ratings of image quality. The ratings are randomly sampled from {1,2,3,4} 
+    #' and randomly distributed across subjects. To introduce a bias in the ratings of defaced images, 
+    #' we add +1 to a predefined precentage of the ratings on original images. The percentage of scan 
+    #' affected varies between raters.
+    #'
+    #' Parameters
+    #' ----------
+    #' n_rated = nbr of subjects rated per rater
+    #' n_sub = nbr of subjects in the dataset
+    #' n_rater = nbr of raters
+    #' perc_biased = vector (1 x n_rater) establishing the percentage of scans biased per rater
+    #' file = filename to save dataset
+    #'
+    #' Returns 
+    #' -------
+    #' df : dataframe containing the manual ratings
+    
+    #Set random seed
+    set.seed(1234)
+
+    manual_original <- matrix(, nrow = n_sub, ncol = n_rater)
+    manual_defaced <- matrix(, nrow = n_sub, ncol = n_rater)
+
+    for (i in 1:n_rater) {
+        #Each rater rates subjects picked at random
+        ind_sub <- sample(1:n_sub, n_rated, replace = F)
+        #random original ratings sampled from {1,2,3,4}
+        ratings <- sample(1:4, n_rated, replace = T)
+        manual_original[ind_sub, i] <- ratings
+
+        #To simulate a positive bias towards defaced data, we improve the ratings of a 
+        #predefined percentage of the original scans
+        ind_rat <- sample(1:n_rated, round(n_rated*perc_biased[i]/100), replace = F)
+        ratings_biased <- ratings
+        ratings_biased[ind_rat] <- ratings_biased[ind_rat] + 1
+        #The scale stops at 4 so clip higher values to 4 
+        ratings_biased[ratings_biased == 5] <- 4
+
+        #Set the biased ratings as the ratings on the defaced condition
+        manual_defaced[ind_sub, i] <- ratings_biased
+    }
+
+    manual_original_vec <- c(manual_original)
+    manual_defaced_vec <- c(manual_defaced)
+
+    defaced <- rep(c(0, 1), times = n_rater*n_sub)
+    sub <- rep(rep(1:n_sub, each=2), times = n_rater)
+    rater <- rep(1:n_rater, each=n_sub*2)
+
+    #Convert to dataframe to use in regression
+    df <- data.frame(sub = sub)
+    df$defaced <- factor(defaced, levels = 0:1, labels = c("original", "defaced"))
+    df$rater <- factor(rater, levels = 1:n_rater, labels = sprintf("rater%02d", 1:n_rater))
+    df$ratings <- factor(c(rbind(manual_original_vec, manual_defaced_vec)), levels = 1:4, labels = c("excluded", "poor", "good", "excellent"))
+
+    #Write dataframe to file
+    if (file != ""){
+    saveRDS(df,file=sprintf("SimulatedData/SimulatedDefacedRatings_%.2fMissing.Rda",(n_sub-n_rated)/n_sub*100))
+    }
+
+    return(df)
+}
