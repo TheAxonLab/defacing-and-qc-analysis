342cb4a614a4e3a02e8dd0d6ee79d257201dc78d
cprovins <celine.provins@unil.ch>
2022-09-12 18:02:18 +0200

    enh : add PCA before MANOVA enh : add site as between-subject factor in MANOVA enh : add site to the simulated IQMs

--- statistical_analysis/IQMs/LoadIQMs.py
+++ statistical_analysis/IQMs/LoadIQMs.py
@@ -33,7 +33,7 @@ def get_key(my_dict,val):
              return key
 
 
-## Load IQMs
+"""## Load IQMs
 iqms_original = np.zeros((n_sub,61))
 iqms_defaced = np.zeros((n_sub,61))
 sub_id = np.zeros((n_sub,1))
@@ -64,7 +64,7 @@ for s in range(0,n_sub*2):
         for i,key in enumerate(iqms_keys):
             iqms_defaced[pos,i] = iqms[key]
     else:
-        raise ValueError("{} is an invalid name".format(sub))
+        raise ValueError("{} is an invalid name".format(sub))"""
 
 
 # +
@@ -74,8 +74,8 @@ iqms_defaced = np.random.rand(n_sub,62)
 
 #Give letters as name
 iqms_keys = [chr(x) for x in range(65, 91)] + [chr(x) for x in range(97, 133)]
-# -
 
+# +
 ## Build dataframe
 sub_id = np.arange(1, n_sub+1)
 sub_id = sub_id[..., np.newaxis]
@@ -87,7 +87,12 @@ i_merge = np.vstack((i_o,i_d))
 #Verify shape matches expectation
 print(i_merge.shape)
 
-iqms_df = pd.DataFrame(i_merge, columns = ['subject'] + iqms_keys + ['defaced'])
+site1 = np.vstack((np.ones((185,1)), np.ones((197,1))*2, np.ones((198,1))*3))
+site = np.vstack((site1, site1))
+print(site.shape)
+# -
+
+iqms_df = pd.DataFrame(np.hstack((i_merge,site)), columns = ['subject'] + iqms_keys + ['defaced'] + ['site'])
 
 # Repeated measure MANOVA is only implemented in R
 # Thus we save the dataframe so we can load it in R
