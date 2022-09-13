1ff5907e18750ae5c68909a345d029d9d5ba77fa
cprovins <celine.provins@unil.ch>
2022-09-13 15:22:24 +0200

    fix : delete useless code to run other type of defacer doc : adapt READ.me fix : correct text in the notebooks so it corresponds to latest decision

--- run_defacing/run_afni_refacer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run -u $( id -u ) -it --rm \
-        -v /data/datasets/DefacingPilotData:/data:ro \
-        -v /data/derivatives/defacing/DefacingPilotData/afni_refacer:/out \
-        pennlinc/afni_refacer  \
-        -input /data   \
-        -mode_all \
-        -prefix /out
