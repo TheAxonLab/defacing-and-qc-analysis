537763222aadaed6688f491808ea029c60c1f209
cprovins <celine.provins@unil.ch>
2023-08-21 21:14:00 +0200

    sty: improve code readability by using pathlib sty: use symlink instead of copying files fix: correct number of subject

--- pilot_study/blind_sub_id.py
+++ pilot_study/blind_sub_id.py
@@ -1,16 +1,13 @@
-import os
 import json
 import random
 from bids import BIDSLayout
-from shutil import copyfile
+from pathlib import Path
 
-# +
-data_path = "/data/datasets/DefacingPilotData/original"
-deface_path = "/data/derivatives/defacing/DefacingPilotData/pydeface/"
-blind_path = "/data/datasets/DefacingPilotData/shuffled"
+data_path = Path("/data/datasets/DefacingPilotData/original")
+deface_path = Path("/data/derivatives/defacing/DefacingPilotData/pydeface/")
+blind_path = Path("/data/datasets/DefacingPilotData/shuffled")
 
-n_sub <- 185 #nbr of subjects available in the dataset
-# -
+n_sub = 10  # nbr of subjects available in the dataset
 
 layout = BIDSLayout(data_path)
 sub_id = layout.get_subjects()
@@ -18,36 +15,86 @@ sub_id.sort()
 
 blind_dict = dict()
 pos_dict = dict()
-shuffle_id = list(range(0,n_sub*2))
+shuffle_id = list(range(1, n_sub * 2 + 1))
 random.shuffle(shuffle_id)
-for i,s in enumerate(sub_id):
-    blind_dict['{}_non_defaced'.format(s)] = shuffle_id[i]
-    blind_dict['{}_pydeface'.format(s)] = shuffle_id[i+n_sub]
-    pos_dict['{}'.format(i)] = s
+for i, s in enumerate(sub_id):
+    blind_dict[f"{s}_non_defaced"] = shuffle_id[i]
+    blind_dict[f"{s}_pydeface"] = shuffle_id[i + n_sub]
+    pos_dict[f"{i}"] = s
 
-    os.makedirs(os.path.join(blind_path,'sub-{}'.format(shuffle_id[i]),'ses-V1','anat'))
-    copyfile(os.path.join(data_path, 'sub-{}'.format(s), "sub-{}_sessions.json".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i]), "sub-{}_sessions.json".format(shuffle_id[i])))
-    copyfile(os.path.join(data_path, 'sub-{}'.format(s), "sub-{}_sessions.tsv".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i]), "sub-{}_sessions.tsv".format(shuffle_id[i])))
-    copyfile(os.path.join(data_path, 'sub-{}'.format(s), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.json".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i]), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.json".format(shuffle_id[i])))
-    copyfile(os.path.join(data_path, 'sub-{}'.format(s), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.nii.gz".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i]), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.nii.gz".format(shuffle_id[i])))
+    (blind_path / f"sub-{shuffle_id[i]:02d}" / "ses-V1" / "anat").mkdir(
+        exist_ok=True, parents=True
+    )
 
-    os.makedirs(os.path.join(blind_path,'sub-{}'.format(shuffle_id[i+10]),'ses-V1','anat'))
-    copyfile(os.path.join(deface_path, 'sub-{}'.format(s), "sub-{}_sessions.json".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i+10]), "sub-{}_sessions.json".format(shuffle_id[i+10])))
-    copyfile(os.path.join(deface_path, 'sub-{}'.format(s), "sub-{}_sessions.tsv".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i+10]), "sub-{}_sessions.tsv".format(shuffle_id[i+10])))
-    copyfile(os.path.join(deface_path, 'sub-{}'.format(s), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w_pydefaced.json".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i+10]), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.json".format(shuffle_id[i+10])))
-    copyfile(os.path.join(deface_path, 'sub-{}'.format(s), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w_pydefaced.nii.gz".format(s)), \
-        os.path.join(blind_path, 'sub-{}'.format(shuffle_id[i+10]), "ses-V1", "anat", "sub-{}_ses-V1_run-1_T1w.nii.gz".format(shuffle_id[i+10])))
+    Path(
+        blind_path / f"sub-{shuffle_id[i]:02d}" / f"sub-{shuffle_id[i]}_sessions.json"
+    ).symlink_to(data_path / f"sub-{s}" / f"sub-{s}_sessions.json")
+    Path(
+        blind_path / f"sub-{shuffle_id[i]:02d}" / f"sub-{shuffle_id[i]}_sessions.tsv"
+    ).symlink_to(data_path / f"sub-{s}" / f"sub-{s}_sessions.tsv")
 
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i]:02d}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{shuffle_id[i]:02d}_ses-V1_run-1_T1w.json"
+    ).symlink_to(
+        data_path / f"sub-{s}" / "ses-V1" / "anat" / f"sub-{s}_ses-V1_run-1_T1w.json"
+    )
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i]:02d}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{shuffle_id[i]:02d}_ses-V1_run-1_T1w.nii.gz"
+    ).symlink_to(
+        data_path / f"sub-{s}" / "ses-V1" / "anat" / f"sub-{s}_ses-V1_run-1_T1w.nii.gz"
+    )
 
-# blind_dict_file = open("DefacingPilotData_blind_dict.json", "w")
-# blind_dict_file = json.dump(blind_dict, blind_dict_file)
+    (blind_path / f"sub-{shuffle_id[i + 10]:02d}" / "ses-V1" / "anat").mkdir(
+        exist_ok=True, parents=True
+    )
 
-pos_dict_file = open("DefacingPilotData_pos_dict.json", "w")
-pos_dict_file = json.dump(pos_dict, pos_dict_file)
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i + 10]:02d}"
+        / f"sub-{shuffle_id[i + 10]:02d}_sessions.json"
+    ).symlink_to(deface_path / f"sub-{s}" / f"sub-{s}_sessions.json")
+
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i + 10]:02d}"
+        / f"sub-{shuffle_id[i + 10]:02d}_sessions.tsv"
+    ).symlink_to(deface_path / f"sub-{s}" / f"sub-{s}_sessions.tsv")
+
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i + 10]:02d}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{shuffle_id[i + 10]:02d}_ses-V1_run-1_T1w.json"
+    ).symlink_to(
+        deface_path
+        / f"sub-{s}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{s}_ses-V1_run-1_T1w_pydefaced.json"
+    )
+    Path(
+        blind_path
+        / f"sub-{shuffle_id[i + 10]:02d}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{shuffle_id[i + 10]:02d}_ses-V1_run-1_T1w.nii.gz"
+    ).symlink_to(
+        deface_path
+        / f"sub-{s}"
+        / "ses-V1"
+        / "anat"
+        / f"sub-{s}_ses-V1_run-1_T1w_pydefaced.nii.gz"
+    )
+
+# Save dictionaries
+Path("DefacingPilotData_blind_dict.json").write_text(json.dumps(blind_dict, indent=2))
+Path("DefacingPilotData_pos_dict.json").write_text(json.dumps(pos_dict, indent=2))
