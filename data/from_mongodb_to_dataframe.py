ee28d0fee90b734d2f117b72f50d3101102075e4
Céline Provins <celine.provins@unil.ch>
2024-05-08 14:54:38 +0200

    fix: correct ratings extraction so that the artifacts list does not get splitted across several columns sty: black

--- data/from_mongodb_to_dataframe.py
+++ data/from_mongodb_to_dataframe.py
@@ -4,15 +4,16 @@ import pandas as pd
 from pymongo.mongo_client import MongoClient
 
 # Path PyMongo to allow using PyMongoArrow’s functionality directly to Collection instances of PyMongo
-from pymongoarrow.monkey import patch_all 
+from pymongoarrow.monkey import patch_all
+
 patch_all()
 
 # Connect to the MongoDB database
-client = MongoClient('localhost', 27017)
-                          
+client = MongoClient("localhost", 27017)
+
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
+    client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
@@ -31,11 +32,14 @@ df = ratings.find_pandas_all({"dataset": "IXI dataset - MRIQC derivatives"})
 # so that comments do not get split into multiple lines
 df["comments"] = df["comments"].str.replace("\n", ", ")
 
-#Drop columns we don't need
-df = df.drop(columns = ['_id', 'md5sum'])
+# Convert 'artifacts' list to a string so that it does not get split over several columns
+df["artifacts"] = df["artifacts"].apply(lambda x: ", ".join(map(str, x)))
+
+# Drop columns we don't need
+df = df.drop(columns=["_id", "md5sum"])
 
 # Save dataframe to csv so we can load it in R
-df.to_csv('IXI_ratings_df.csv')
+df.to_csv("IXI_ratings_df.csv")
 
 # Close the connection
 client.close()
